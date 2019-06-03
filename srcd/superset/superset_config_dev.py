d1b40012a4a0126db1f2093f004a27032f629220
Maxim Sukharev <max@smacker.ru>
2019-06-03 15:08:18 +0200

    make code pass pylint
    - bblfsh.views uses the same disable rules as views/core
    - disable warning of importing bblfsh views in config
    
    Signed-off-by: Maxim Sukharev <max@smacker.ru>

--- srcd/superset/superset_config_dev.py
+++ srcd/superset/superset_config_dev.py
@@ -3,7 +3,8 @@ BBLFSH_WEB_ADDRESS = 'http://127.0.0.1:9999'
 WTF_CSRF_EXEMPT_LIST = ['superset.bblfsh.views.api']
 
 
-def mutator(f):
+def mutator():
+    # pylint: disable=unused-variable
     from superset.bblfsh import views  # noqa
 
 
