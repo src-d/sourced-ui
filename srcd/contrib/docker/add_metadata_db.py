c4737e12968fdeacb1cb1be74a3e545b2e21450e
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-06-21 15:28:43 +0200

    Add metadata tables
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- srcd/contrib/docker/add_metadata_db.py
+++ srcd/contrib/docker/add_metadata_db.py
@@ -1,4 +1,5 @@
 from superset import conf, db
+from superset.connectors.connector_registry import ConnectorRegistry
 from superset.models import core as models
 
 
@@ -18,4 +19,30 @@ def get_or_create_metadata_db():
     return dbobj
 
 
+def add_metadata_tables():
+    schema = conf.get('METADATA_DB')
+    dbobj = get_or_create_metadata_db()
+    TBL = ConnectorRegistry.sources['table']
+    for table in dbobj.all_table_names_in_schema(schema):
+        # table_name should match the one in the datasource for fetch_metadata to work
+        if db.session.query(TBL).filter_by(table_name=table).first():
+            continue
+        if db.session.query(TBL).filter_by(table_name='%s.%s' % (schema, table)).first():
+            continue
+
+        # create table with original name and fetch columns
+        tbl = TBL(table_name=table)
+        tbl.database = dbobj
+        db.session.add(tbl)
+        db.session.commit()
+        tbl.fetch_metadata()
+
+        # rename with prefix and set source
+        tbl.table_name = '%s.%s' % (schema, table)
+        tbl.sql = 'select * from ' + table
+        db.session.add(dbobj)
+        db.session.commit()
+
+
 get_or_create_metadata_db()
+add_metadata_tables()
