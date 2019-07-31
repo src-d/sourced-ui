9889bf4d1a323ff39f3eddb992fd4652ffeb0013
David Pordomingo <David.Pordomingo.F@gmail.com>
2019-07-31 21:58:19 +0200

    Make srcdMain the default palette for SUPERSET_DEFAULT key
    Superset is using 'SUPERSET_DEFAULT' as the default CategoricalScheme key
    i.e. https://github.com/apache-superset/superset-ui/blob/master/packages/superset-ui-color/src/CategoricalSchemeRegistrySingleton.ts
    This commit assigns 'srcdMain' palette for 'SUPERSET_DEFAULT' key
    
    Signed-off-by: David Pordomingo <David.Pordomingo.F@gmail.com>

--- srcd/superset/assets/src/customization/index.jsx
+++ srcd/superset/assets/src/customization/index.jsx
@@ -1,12 +1,18 @@
 import { getCategoricalSchemeRegistry } from '@superset-ui/color';
 import srcdColors from './srcdColors';
 
+export const defaultScheme = 'SUPERSET_DEFAULT';
+
+const srcdPaletteId = 'srcdMain';
 
 function setupColors() {
   const categoricalSchemeRegistry = getCategoricalSchemeRegistry();
   [srcdColors].forEach((group) => {
     group.forEach((scheme) => {
       categoricalSchemeRegistry.registerValue(scheme.id, scheme);
+      if (scheme.id === srcdPaletteId) {
+        categoricalSchemeRegistry.registerValue(defaultScheme, scheme);
+      }
     });
   });
 }
