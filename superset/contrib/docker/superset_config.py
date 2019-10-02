831c3a095d6a0d7b3f9d4299dcaa91588c27b70b
Alex Corcoles <alex@pdp7.net>
2019-10-02 15:42:40 +0200

    Add support for configuring Google OAuth logins
    Known issues:
    
    * Upon registration/login you are redirected to an ugly error message.
      If you change the URL you can access the application and things are
      working, though
    
    * User cannot access the user list!
    
    * No documentation for the new environment variables
    
    Signed-off-by: Alex Corcoles <alex@pdp7.net>

--- superset/contrib/docker/superset_config.py
+++ superset/contrib/docker/superset_config.py
@@ -152,3 +152,34 @@ FLASK_APP_MUTATOR = mutator
 # This parameter documented as deprecated but frontend still uses it
 # Should match `--timeout` value of gunicorn
 SUPERSET_WEBSERVER_TIMEOUT = 300
+
+OAUTH_PROVIDER = get_env_variable('OAUTH_PROVIDER', False)
+
+if OAUTH_PROVIDER:
+    from flask_appbuilder.security.manager import AUTH_OAUTH
+
+    AUTH_TYPE = AUTH_OAUTH
+    AUTH_USER_REGISTRATION = True
+    AUTH_USER_REGISTRATION_ROLE = get_env_variable('OAUTH_REGISTRATION_ROLE')
+
+    if OAUTH_PROVIDER == 'google':
+        OAUTH_PROVIDERS = [
+            {
+                'name': 'google',
+                'icon': 'fa-google',
+                'token_key': 'access_token',
+                'remote_app': {
+                    'consumer_key': get_env_variable('OAUTH_CONSUMER_KEY'),
+                    'consumer_secret': get_env_variable('OAUTH_CONSUMER_SECRET'),
+                    'base_url': 'https://www.googleapis.com/oauth2/v2/',
+                    'request_token_params': {
+                        'scope': 'email profile'
+                    },
+                    'request_token_url': None,
+                    'access_token_url': 'https://accounts.google.com/o/oauth2/token',
+                    'authorize_url': 'https://accounts.google.com/o/oauth2/auth'
+                }
+            }
+        ]
+    else:
+        raise EnvironmentError('Unknown OAuth provider {}'.format(OAUTH_PROVIDER))
